10bd433536d46a0a35592a6bc0dcc0ea517b1ff2
Garrick Chan <changarrickt@gmail.com>
2026-02-14 18:16:16 -0800

    Added bar chart and click events

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@
 import ReactDom from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './Colony.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(<App />)
